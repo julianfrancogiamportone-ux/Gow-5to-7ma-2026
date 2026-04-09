00c40b2155e9e617f98ee54faa8f9ab599285ed3
Maximo <maximonovicovcamargo@gmail.com>
2026-04-09 13:06:23 -0300

    peleador

--- src/main/java/GOW/entidades/Peleador.java
+++ src/main/java/GOW/entidades/Peleador.java
@@ -1,5 +1,11 @@
 package GOW.entidades;
-
+import jakarta.persistence.*;
+@Entity
 public class Peleador {
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long Id;
+private String nombre;
+private int PuntosVida;
+private int energia;
 }
