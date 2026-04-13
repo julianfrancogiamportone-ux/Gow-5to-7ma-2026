098642b98d542e1a3f6be2478f83c513339d66a7
laureano <laureanofunes440@gmail.com>
2026-04-13 08:16:19 -0300

    Arma

--- src/main/java/GOW/entidades/Arma.java
+++ src/main/java/GOW/entidades/Arma.java
@@ -6,7 +6,36 @@ private int id;
 private String nombre;
 private int BonificadorDanio;
 private float peso;
-public Arma(String nombre; int BonificadorDanio){
-this.nombre = nombre;
-this.BonificadorDanio = BonificadorDanio;
+public Arma() {
+}
+public Arma(int id, String nombre, int bonificadorDanio, float peso) {
+    this.id = id;
+    this.nombre = nombre;
+    BonificadorDanio = bonificadorDanio;
+    this.peso = peso;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getNombre() {
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public int getBonificadorDanio() {
+    return BonificadorDanio;
+}
+public void setBonificadorDanio(int bonificadorDanio) {
+    BonificadorDanio = bonificadorDanio;
+}
+public float getPeso() {
+    return peso;
+}
+public void setPeso(float peso) {
+    this.peso = peso;
+}
 }
